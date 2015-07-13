0525ee5cca3c5bec82f0086bf7892aff8c59fec5
Petro Koriakin <pkor@softserveinc.com>
2015-07-13 21:09:35 +0300

    another one refactoring iteration

--- library/commentable.rb
+++ library/commentable.rb
@@ -6,6 +6,7 @@ module Library
     def self.included(base)
       base.send :extend, ClassMethods
       base.send :prepend, Initializer
+      base.send :class_variable_set, :@@comments_counter, 0
     end
 
     module ClassMethods
@@ -14,19 +15,14 @@ module Library
         self.class_variable_get :@@comments_counter
       end
 
-      def inc_comment_or_init increment = 0
-        if self.class_variables.include? :@@comments_counter
-          self.class_variable_set :@@comments_counter, self.class_variable_get(:@@comments_counter) + increment
-        else
-          self.class_variable_set :@@comments_counter, 0 
-        end
+      def inc_comment_or_init increment
+        self.class_variable_set :@@comments_counter, self.class_variable_get(:@@comments_counter) + increment
       end
 
     end
 
     module Initializer
       def initialize *args
-        self.class.inc_comment_or_init
         @comments = []
         super *args
       end
